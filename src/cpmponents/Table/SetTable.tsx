869cf57b32bb7ef23b872f225acf58680f0df624
A_ito <BoxPistols@users.noreply.github.com>
2022-11-04 01:50:51 +0900

    Set Table 分離
    Co-authored-by: ai <ai@ex.in>

---
+++ src/cpmponents/Table/SetTable.tsx
@@ -0,0 +1,55 @@
+// データを持ち、表示させるプレゼンテーショナルコンポーネント
+import { useState } from 'react';
+import { BasicTable } from './Basic';
+import { Search } from './Search';
+/**
+ * searched?: any;
+ * initialRows?: any;
+ * setRows?: any;
+ * setSearched?: any;
+ */
+
+// データの型
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number
+) {
+  // 配列で返す
+  return { name, calories, fat, carbs, protein };
+}
+
+// TODO: json化 / データの投入
+const initialRowsData = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Frozen Juice', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export const SetTable = () => {
+  // テーブルの行
+  const [rowsData, setRowsData] = useState([...initialRowsData]);
+  // 検索バーに入力された文字
+  const [searchedData, setSearchedData] = useState('');
+
+  return (
+    <>
+      {/*　検索とテーブルのデータ共有 */}
+      <Search
+        initialRows={initialRowsData} // テーブルデータ
+        setRows={setRowsData} // 検索対象
+        searched={searchedData} // 検索結果
+        setSearched={setSearchedData} // トリガー
+        id='1'
+        label='検索'
+      />
+      {/* テーブル行の設定 */}
+      <BasicTable rows={rowsData} />
+    </>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { SetTable } from './cpmponents/Table/SetTable';
+// import { BasicTable } from './cpmponents/Table/Basic';
 import './index.css';
-import { SetTable } from './cpmponents/Basic'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
